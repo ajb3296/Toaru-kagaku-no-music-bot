c6b67b3387448dfdc42d558e184a8f180417bd01
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2020-10-24 10:07:32 +0900

    Update crawler.py

--- musicbot/utils/crawler.py
+++ musicbot/utils/crawler.py
@@ -26,10 +26,3 @@ async def getReq (url : str, header : str = None) :
     async with aiohttp.ClientSession (headers=header) as session:
         async with session.get (url) as r :
             return r
-         
-async def checkDevice (device : str) :
-    data = await getReqJSON ('https://raw.githubusercontent.com/LineageOS/hudson/master/updater/devices.json')
-    for x in data :
-        if device in x["model"] :
-            return x
-    return False
