0cbba1af100a867309fa785de85d2b23b5274c6e
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2021-01-30 20:45:30 +0900

    볼륨 아이콘 변경 버그 수정

--- musicbot/cogs/music.py
+++ musicbot/cogs/music.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 async def volumeicon(vol : int):
     if vol >= 1 and vol <= 10:
         volicon = ":mute:"
-    if vol >= 11 and vol <= 30:
+    elif vol >= 11 and vol <= 30:
         volicon = ":speaker:"
     elif vol >= 31 and vol <= 70:
         volicon = ":sound:"
