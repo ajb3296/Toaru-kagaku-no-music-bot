3bdb035aff42daac6c56a2468c3008e621e73ae7
An Jaebeom <ajb8533296@gmail.com>
2022-02-04 00:03:39 +0900

    Add comment

--- file_name.py
+++ file_name.py
@@ -8,11 +8,12 @@ anilist_path = "musicbot/anilist"
 
 files = []
 for file in os.listdir(anilist_path):
-    if file.endswith(".txt"):
-        files.append(file)
+    #if file.endswith(".txt"):
+    files.append(file)
 
 for file in files:
     file_oldname = os.path.join(anilist_path, file)
+    # remove \ / : * ? " < > |
     file_newname_newfile = os.path.join(anilist_path, re.sub("\\|\/|\:|\*|\?|\"|\<|\>|\|","", file))
 
     os.rename(file_oldname, file_newname_newfile)
