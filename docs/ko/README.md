c724098fecb2c78919ba23d94495d9fe3a8bc584
An Jaebeom <ajb8533296@gmail.com>
2022-03-20 01:05:32 +0900

    시간차 에러 수정

---
+++ docs/ko/README.md
@@ -0,0 +1,1 @@
+# Toaru kagaku no musicbot
--- musicbot/utils/statistics.py
+++ musicbot/utils/statistics.py
@@ -14,36 +14,38 @@ statistics_db_path = "statistics.db"
 
 class Statistics:
     def up(video_id):
+        # Set table name
+        table_name = f"date{datetime.today().strftime('%Y%m%d')}"
         # Get play count from db
         statisticsdb = Statistics_Db
-        temp = statisticsdb.get(video_id)
+        temp = statisticsdb.get(table_name, video_id)
         # Set count
         if temp is None:
             num = 1
         else:
             num = temp[2] + 1
-        statisticsdb.write(video_id, num)
+        statisticsdb.write(table_name, video_id, num)
 
     # 이게 왜 필요했더라...언젠간 쓰겠지
     def down(video_id):
-        # if the statistics_db_path file exists
-        if os.path.exists(statistics_db_path):
-            # Get play count from db
-            statisticsdb = Statistics_Db
-            temp = statisticsdb.get(video_id)
-            # Set count
-            if temp is None:
-                num = 1
-            else:
-                num = temp[2] - 1
-            statisticsdb.write(video_id, num)
+        # Set table name
+        table_name = f"date{datetime.today().strftime('%Y%m%d')}"
+        # Get play count from db
+        statisticsdb = Statistics_Db
+        temp = statisticsdb.get(table_name, video_id)
+        # Set count
+        if temp is None:
+            num = 1
+        else:
+            num = temp[2] - 1
+        statisticsdb.write(table_name, video_id, num)
 
 class Statistics_Db:
-    def get(video_id):
+    def get(table_name, video_id):
         conn = sqlite3.connect(statistics_db_path, isolation_level=None)
         c = conn.cursor()
         try:
-            c.execute(f"SELECT * FROM date{datetime.today().strftime('%Y%m%d')} WHERE video_id=:Id", {"Id": video_id})
+            c.execute(f"SELECT * FROM {table_name} WHERE video_id=:Id", {"Id": video_id})
         except sqlite3.OperationalError:
             conn.close()
             return None
@@ -63,18 +65,18 @@ class Statistics_Db:
         conn.close()
         return temp
 
-    def write(video_id, edit_count = 1):
+    def write(table_name, video_id, edit_count = 1):
         # Create table if it doesn't exist
         conn = sqlite3.connect(statistics_db_path, isolation_level=None)
         c = conn.cursor()
-        c.execute(f"CREATE TABLE IF NOT EXISTS date{datetime.today().strftime('%Y%m%d')} (id integer PRIMARY KEY AUTOINCREMENT, video_id text, count int)")
+        c.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (id integer PRIMARY KEY AUTOINCREMENT, video_id text, count int)")
 
         if edit_count == 1:
             # add music count
-            c.execute(f"INSERT INTO date{datetime.today().strftime('%Y%m%d')} (video_id, count) VALUES('{video_id}', {edit_count})")
+            c.execute(f"INSERT INTO {table_name} (video_id, count) VALUES('{video_id}', {edit_count})")
         else:
             # modify music count
-            c.execute(f"UPDATE date{datetime.today().strftime('%Y%m%d')} SET count=:count WHERE video_id=:video_id", {"count": edit_count, 'video_id': video_id})
+            c.execute(f"UPDATE {table_name} SET count=:count WHERE video_id=:video_id", {"count": edit_count, 'video_id': video_id})
         conn.close()
 
 if __name__ == "__main__":
