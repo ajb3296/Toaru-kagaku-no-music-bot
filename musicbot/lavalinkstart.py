d13739fe5d9ca5fd471a3d52c0f89f807f19ce51
An Jaebeom <ajb8533296@gmail.com>
2022-09-22 15:09:06 +0900

    edit java path

--- musicbot/lavalinkstart.py
+++ musicbot/lavalinkstart.py
@@ -1,6 +1,7 @@
-import os
+from musicbot.utils.get_java_path import get_java_path
+import subprocess
 import multiprocessing
 
 def child_process():
     print(f"Child process PID : {multiprocessing.current_process().pid}")
-    os.system("java -jar Lavalink.jar")
+    subprocess.call([get_java_path(), '-jar', 'Lavalink.jar'])
