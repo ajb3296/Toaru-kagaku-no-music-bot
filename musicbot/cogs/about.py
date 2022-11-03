f3ef7412817bf5fa96132c7fe687d58886d53be8
An Jaebeom <ajb8533296@gmail.com>
2022-11-03 23:27:28 +0900

    edit about

--- musicbot/cogs/about.py
+++ musicbot/cogs/about.py
@@ -12,6 +12,15 @@ class About (commands.Cog) :
     @slash_command()
     async def about (self, ctx):
         """ Let me tell you about me! """
+        player_server_count = 0
+        for i in self.bot.guilds:
+            player = self.bot.lavalink.player_manager.get(int(i.id))
+            try:
+                if player.is_connected:
+                    player_server_count += 1
+            except Exception:
+                pass
+
         players = 0
         playing_players = 0
         for node in self.bot.lavalink.node_manager.nodes:
@@ -20,8 +29,11 @@ class About (commands.Cog) :
             playing_players += stats.playing_players
 
         embed=discord.Embed(title=get_lan(ctx.author.id, "about_bot_info"), description=AboutBot, color=color_code)
+        embed.add_field(name="Github", value="[https://github.com/ajb3296/Toaru-kagaku-no-music-bot](<https://github.com/ajb3296/Toaru-kagaku-no-music-bot>)", inline=False)
         embed.add_field(name=get_lan(ctx.author.id, "about_guild_count"), value=len(self.bot.guilds), inline=True)
-        embed.add_field(name=get_lan(ctx.author.id, "about_number_of_music_playback_servers"), value=f"{players}({playing_players} playing)", inline=True)
+        embed.add_field(name=get_lan(ctx.author.id, "about_number_of_music_playback_servers"),
+            value=f"lavalink: {players}({playing_players} playing)\nvoice channel count: {player_server_count} playing",
+            inline=True)
         embed.set_footer(text=BOT_NAME_TAG_VER)
         await ctx.respond(embed=embed)
 
