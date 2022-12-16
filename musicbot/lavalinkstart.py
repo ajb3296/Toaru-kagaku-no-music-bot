ce2bab0c7252f731fd5f2691b915c8604da4178f
JaeBeom An <ajb8533296@gmail.com>
2022-12-16 23:24:01 +0900

    edit lavalinkstart

--- musicbot/lavalinkstart.py
+++ musicbot/lavalinkstart.py
@@ -3,4 +3,4 @@ import multiprocessing
 
 def child_process():
     print(f"Child process PID : {multiprocessing.current_process().pid}")
-    subprocess.call("java -jar Lavalink.jar", shell=False)
+    subprocess.call(["java", "-jar", "Lavalink.jar"], shell=False)
