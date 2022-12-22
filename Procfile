0ac05d74f7175f5d457e77025dddd2f4f9d60137
An Jaebeom <ajb8533296@gmail.com>
2022-12-23 01:59:26 +0900

    Remove Heroku related code

--- Procfile
+++
@@ -1,2 +0,0 @@
-ps:scale web=0 worker=1
-worker: python3 -m musicbot
---
+++ dashboard/modules/database.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+class Database:
+    def __init__(self):
+        self.db_path = "statistics.db"
+    
+    def get(self, table_name, video_id):
+        conn = sqlite3.connect(self.db_path, isolation_level=None)
+        c = conn.cursor()
+        try:
+            c.execute(f"SELECT * FROM {table_name} WHERE video_id=:video_id", {"video_id": video_id})
+        except sqlite3.OperationalError:
+            conn.close()
+            return None
+        temp = c.fetchone()
+        conn.close()
+        return temp
