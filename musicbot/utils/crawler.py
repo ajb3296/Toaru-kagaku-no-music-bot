df858121f7ea3dde6029a3906d82c1f69d27193b
An Jaebeom <ajb8533296@gmail.com>
2024-09-23 14:38:58 +0900

    error fix

--- musicbot/utils/crawler.py
+++ musicbot/utils/crawler.py
@@ -1,33 +1,33 @@
 import aiohttp
 import json
 
-connector = aiohttp.TCPConnector(verify_ssl=False)
-
-
 async def getReqJSON(url: str):
-    async with aiohttp.ClientSession() as session:
+    connector = aiohttp.TCPConnector(ssl=False)
+    async with aiohttp.ClientSession(connector=connector) as session:
         async with session.get(url) as r:
             data = await r.read()
         return json.loads(data)
 
 
 async def getReqTEXT(url: str, header: (dict | None) = None) -> str:
+    connector = aiohttp.TCPConnector(ssl=False)
     if header is None:
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(connector=connector) as session:
             async with session.get(url=url) as r:
                 data = await r.text()
             return data
-    async with aiohttp.ClientSession(headers=header) as session:
+    async with aiohttp.ClientSession(connector=connector, headers=header) as session:
         async with session.get(url=url) as r:
             data = await r.text()
         return data
 
 
 async def getReq(url: str, header: (dict | None) = None):
+    connector = aiohttp.TCPConnector(ssl=False)
     if header is None:
-        async with aiohttp.ClientSession() as session:
+        async with aiohttp.ClientSession(connector=connector) as session:
             async with session.get(url) as r:
                 return r
-    async with aiohttp.ClientSession(headers=header) as session:
+    async with aiohttp.ClientSession(connector=connector, headers=header) as session:
         async with session.get(url) as r:
             return r
